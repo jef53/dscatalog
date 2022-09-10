aa45739f7610f26b2688562a10254bd8e39d0632
jef53 <jef.pdy@gmail.com>
2022-09-10 13:38:53 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,14 @@
+import './assets/styles/custom.scss'
 import './App.css';
+import { Navbar } from 'components/Navbar';
+
 
 function App() {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <Navbar />
+      <h1 >Hello DSCatalog</h1>
+    </>
   );
 }
 
